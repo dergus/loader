4ddc8b4cb7b0feaf6423d52f898713597e773c56
Ziyadin Shemsedinov <dergesus@gmail.com>
2018-11-03 12:05:12 +0300

    init

---
+++ database/migrations/2018_11_03_085613_create_table_downloads.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTableDownloads extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('downloads', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('url', 5000);
+            $table->tinyInteger('status');
+            $table->string('storage_path');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('downloads');
+    }
+}
